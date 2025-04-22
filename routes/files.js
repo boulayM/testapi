0dc509cfb0c6490c36b7930f59c69173b29e083a
Boulay.M <macboulay2@gmail.com>
2025-04-22 19:08:24 +0200

    add files

--- routes/files.js
+++ routes/files.js
@@ -4,8 +4,9 @@ const service = require ('../services/files');
 const multer = require ('../middlewares/files-storage');
 const private = require ('../middlewares/private');
 
+
 router.get('/', private.chckeJWT, service.getAllFiles);
-router.post('/', multer, createOneFile);
+router.post('/', multer, service.createOneFile);
 router.get('/:id', private.chckeJWT, service.getOneFile);
 router.put('/:id', private.chckeJWT, multer, service.modifyOneFile);
 router.delete('/delete', private.chckeJWT, service.deleteOneFile);
